68c93387d304fc0fcd48a11de7192aa62a4d5e70
Lily <dmsruddlvhs2013@gmail.com>
2021-01-06 10:14:55 +0900

    bool_type

--- boolDemo.py
+++ boolDemo.py
@@ -3,3 +3,29 @@
 # True(T), False(F)
 # not, and, or -> 논리연산자
 # &, |, ~ -> 비교연산자
+
+# False로 간주
+# "" , [], (), {}, 숫자(0 이외의 숫자는 True), None
+
+# 2진수로 표현 -> 0101 & 0000 -> 0000 -> 0
+xValue = 5
+yValue = 0
+print(xValue & yValue)
+print(bool(xValue & yValue))
+# 2진수로 표현 -> 0101 & 0000 -> 0101 -> 5
+print(xValue | yValue)
+print(bool(xValue | yValue))
+
+trueValue = True
+falseValue = False
+
+print(int(trueValue))
+print(int(falseValue))
+
+print(trueValue & falseValue)
+print(trueValue | falseValue)
+
+print('and - ', trueValue and falseValue)
+print('or - ', trueValue or falseValue)
+print('not - ', not trueValue)
+
