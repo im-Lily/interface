20c8b3054aae3a90a01566413f3d375b00ecbfbc
Lily <dmsruddlvhs2013@gmail.com>
2021-01-06 16:42:09 +0900

    date_type

--- dateDemo.py
+++ dateDemo.py
@@ -2,7 +2,6 @@
 
 from datetime import date, datetime, timedelta
 
-
 today = date.today()
 print('date - ', type(today), today, today.year, today.month, today.day)
 print('년도 {}, 월 {}, 일 {}'.format(today.year, today.month, today.day))
