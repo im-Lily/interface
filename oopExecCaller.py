5b63a15d74af46c3bb01f21c9ab89f2b6394c491
im-Lily <dmsruddlvhs2013@gmail.com>
2021-01-11 16:29:13 +0900

    oopCaller

---
+++ oopExecCaller.py
@@ -0,0 +1,54 @@
+from ai.oop.oopExec import *
+
+# unit = Unit(10,100)
+# unit.unitInfo()
+#
+# # type check
+# if unit.utype == 'Unit' :
+#     print(True)
+# else :
+#     print(False)
+
+# Marin 생성
+marin01 = Marine(10, 100, 0, 0)
+marin02 = Marine(10, 100, 0, 0)
+marin03 = Marine(10, 100, 0, 0)
+marin04 = Marine(10, 100, 0, 0)
+
+# Medic 생성
+medic01 = Medic(0,100,0)
+medic02 = Medic(0,100,0)
+
+# 병력을 list에 담아줌
+tropList = []
+tropList.append(marin01)
+tropList.append(marin02)
+tropList.append(marin03)
+tropList.append(marin04)
+tropList.append(medic01)
+tropList.append(medic02)
+
+# 기본정보 출력
+for obj in tropList :
+    obj.unitInfo()
+
+# DropShip 생성
+ship = DropShip(0, 50)
+
+# 부대원을 태운다면?
+ship.board(tropList)
+
+# 목표지점으로 이동
+ship.attack()
+
+# 부대원 하차
+tropAttackList = ship.drop()
+
+# 공격
+for unit in tropAttackList :
+    if isinstance(unit, Marine) :
+        unit.stimpack()
+    unit.attack()
+
+
+
